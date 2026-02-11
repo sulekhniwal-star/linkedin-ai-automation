be17e06d2d5cc5e589721d547213f68bcfecf6d1
Sulekh Niwal <sulekhniwal@gmail.com>
2026-02-11 14:45:20 +0530

    Update model name in chat completion

--- post.py
+++ post.py
@@ -77,7 +77,7 @@ existing_hashes = load_hashes()
 
 for _ in range(5):
     response = client.chat.completions.create(
-        model="llama3-70b-8192",
+        model="llama-3.3-70b-versatile",
         messages=[{"role": "user", "content": PROMPT}]
     )
 
